1c9532f2c07dcc51ba21fc800a2f4b1bcd6e6650
Nazarov Yuriy <nazarov.yuriy.pavlovich@gmail.com>
2013-10-06 21:09:40 +0400

    Win32::Daemon added.

--- ftp-dir-syncd.pl
+++ ftp-dir-syncd.pl
@@ -3,10 +3,22 @@ use Getopt::Long;
 use Net::FTP;
 use Config::IniFiles;
 use Data::Dumper;
-use strict;
+use Pod::Usage;
+use strict	qw(refs vars);
 use warnings;
 
-#Global variables
+BEGIN {
+	if ( $^O eq 'MSWin32' ) {
+		require Win32::Daemon;
+		Win32::Daemon->import();
+	}
+}
+
+#
+#
+# Global variables
+#
+#
 use constant {
 	DAEMON_VERSION    => "0.1",
 	DEFAULT_User      => "anonymous",
@@ -19,16 +31,107 @@ use constant {
 	DEFAULT_DifDate   => "yes",
 	DEFAULT_DifSize   => "yes",
 };
-my @config_search_paths = ( '/etc/ftp-dir-sync.conf', './ftp-dir-sync.conf', './ftp-dir-sync.ini');
+my @config_search_paths = (
+	'/etc/ftp-dir-sync.conf',
+	'./ftp-dir-sync.conf',
+	'./ftp-dir-sync.ini'
+);
 
-sub print_usage() {
-	print "Use:
-$0 --help		to view help
-$0 --version		to view version
-$0 --daemon		to start as daemon
-";
-	return 1;
+my $config_hash_ref;
+
+#
+#
+# Platform specific code
+#
+#
+
+my %service_context = (
+	'last_state' => SERVICE_STOPPED,
+	'start_time' => time(),
+);
+
+my %service_properties_hash = (
+	'name'        => 'ftp-dir-sync service',
+	'description' => 'keep local and remote directories in sync',
+	'display'     => 'ftp-dir-sync service',
+	'path'        => $^X,
+	'user'        => ".\\user",
+	'password'    => "",
+	'parameters'  => "\"$0\" --daemon",
+);
+
+sub install() {
+	if ( Win32::Daemon::CreateService( \%service_properties_hash ) ) {
+		print "Installed";
+	}
+	else {
+		print "Error " . Win32::Daemon::GetLastError();
+		exit 1;
+	}
 }
+
+sub uninsall() {
+	if ( Win32::Daemon::DeleteService( $service_properties_hash{'name'} ) ) {
+		print "Uninstalled";
+	}
+	else {
+		print "Error " . Win32::Daemon::GetLastError();
+		exit 1;
+	}
+}
+
+sub service_callback_start() {
+	my( $Event, $Context ) = @_;
+	$service_context{last_state} = SERVICE_RUNNING;
+	Win32::Daemon::State( SERVICE_RUNNING );
+}
+
+sub service_callback_running() {
+	my( $Event, $Context ) = @_;
+	if( SERVICE_RUNNING == Win32::Daemon::State() ) {
+		eval { download_files($config_hash_ref); };
+	}
+}
+
+sub service_callback_stop() {
+	my( $Event, $Context ) = @_;
+	$service_context{'last_state'} = SERVICE_RUNNING;
+	Win32::Daemon::State( SERVICE_RUNNING );
+}
+
+sub service_callback_pause() {
+	my( $Event, $Context ) = @_;
+	$service_context{'last_state'} = SERVICE_RUNNING;
+	Win32::Daemon::State( SERVICE_RUNNING );
+}
+
+sub service_callback_continue() {
+	my( $Event, $Context ) = @_;
+	$service_context{'last_state'} = SERVICE_STOPPED;
+	Win32::Daemon::State( SERVICE_STOPPED );
+}
+
+sub setup_service {
+	Win32::Daemon::RegisterCallbacks({
+		start    => \&service_callback_start,
+		running  => \&service_callback_running,
+		stop     => \&service_callback_stop,
+		pause    => \&service_callback_pause,
+		continue => \&service_callback_continue,
+	}) or error("register callbacks failed\n");
+	Win32::Daemon::StartService( \%service_context, 2000 );
+	exit 0;
+}
+
+#
+#
+# Platform independent code
+#
+#
+
+#
+# Utils
+#
 
 sub error($) {
 	my ($message) = @_;
@@ -38,17 +141,6 @@ sub error($) {
 sub print_to_log($) {
 	my ($message) = @_;
 	print "$message\n";	#ToDo: remove it after debug completed.
-}
-
-sub print_version() {
-	print "ftp-dir-sync daemon version " . DAEMON_VERSION . "\n";
-	return 0;
-}
-
-sub print_help() {
-	print_version();
-	print_usage();
-	return 0;
 }
 
 sub read_config($) {
@@ -67,15 +159,18 @@ sub read_config($) {
 	);
 	my %ini;
 	tie %ini, 'Config::IniFiles', ( -file => $config_file_path );
-	for my $k (keys %{$ini{'global'}}){
+	for my $k (keys %{$ini{'global'}}) {
 		$config{$k} = $ini{'global'}{$k};
 	}
 	return \%config;
 }
 
-sub daemonize {
-	use POSIX;
-	POSIX::setsid or die "setsid: $!";
+sub print_usage() {
+	pod2usage(2);
+	return 1;
+}
+
+sub daemonize() {
 	my $pid = fork();
 	if ( $pid < 0 ) {
 		error "Daemonization failed.";
@@ -88,64 +183,86 @@ sub daemonize {
 	open( STDERR, ">", "/dev/null" );
 }
 
-sub check_is_config_valid($$) {
-	my ( $config_hash_ref, $message_str_ref ) = @_;
-	return 1;
+#
+# Logic
+#
+
+sub print_version() {
+	print "ftp-dir-sync daemon version " . DAEMON_VERSION . "\n";
+	return 0;
 }
 
-sub download_files($) {
-	my ($config_hash_ref) = @_;
-	
+sub print_help() {
+	print_version();
+	pod2usage(1);
+	return 0;
+}
+
+sub print_man(){
+	pod2usage( -verbose => 2 );
+	return 0;
+}
+
+sub download_files() {
 	my $use_passive = !exists $config_hash_ref->{'FTPType'}
 	  || $config_hash_ref->{'FTPType'} eq 'passive';
 	my $host = $config_hash_ref->{'Host'};
-	my $ftp = Net::FTP->new(
+	my $ftp  = Net::FTP->new(
 		$host,
 		Debug   => 0,
 		Passive => $use_passive,
 		Timeout => 10,
 	) or die "Cannot connect to $host: $@";
-	
-	$ftp->login(
-		$config_hash_ref->{'User'},
-		$config_hash_ref->{'Password'}
-	) or die "Cannot login ", $ftp->message;
-	
+
+	$ftp->login( $config_hash_ref->{'User'},
+		$config_hash_ref->{'Password'} )
+	  or die "Cannot login ", $ftp->message;
+
 	my $remote_path = $config_hash_ref->{'FTPPath'};
-	my $local_path = $config_hash_ref->{'LocalPath'};
-	my @files = $ftp->ls($remote_path);
-	
-	for my $file (@files){
-		print "getting file $file\n";	#ToDo: remove it after debug completed.
-		$ftp->get($remote_path.'/'.$file, $local_path.'/'.$file);
+	my $local_path  = $config_hash_ref->{'LocalPath'};
+	my @files       = $ftp->ls($remote_path);
+
+	for my $file (@files) {
+		print "getting file $file\n";    #ToDo: remove it after debug completed.
+		$ftp->get(
+			$remote_path . '/' . $file,
+			$local_path . '/' . $file
+		);
 	}
 }
 
-sub run_daemon($) {
-	my ($config_hash_ref) = @_;
-	daemonize();
+sub run_daemon() {
+	daemonize() if is_linux();
 
 	while (1) {
-		eval { download_files($config_hash_ref); };
+		eval { download_files(); };
 		if ( defined $@ ) {
 			print_to_log($@);
 		}
-		print_to_log("Files were downloaded. Waiting ".$config_hash_ref->{'Period'}." s.");
+		print_to_log( "Files were downloaded. Waiting "
+			  . $config_hash_ref->{'Period'}
+			  . " s." );
 		sleep $config_hash_ref->{'Period'};
 	}
 }
 
 sub main() {
-	my ( $config_file_path, $daemon, $help, $version );
+	my ( $config_file_path, $daemon, $help, $man, $version, $install, $uninstall );
 	GetOptions(
-		"config:s" => \$config_file_path,
-		"daemon"   => \$daemon,
-		"help"     => \$help,
-		"version"  => \$version,
+		"config:s"  => \$config_file_path,
+		"daemon"    => \$daemon,
+		"help"      => \$help,
+		"man"       => \$man,
+		"version"   => \$version,
+		"install"   => \$install,  #ToDo: remove this from linux version
+		"uninstall" => \$uninstall,
 	);
 
 	if ($help) {
 		exit print_help();
+	}
+	elsif ($man) {
+		exit print_man();
 	}
 	elsif ($version) {
 		exit print_version();
@@ -159,9 +276,9 @@ sub main() {
 		unless ( defined $config_file_path ) {
 			error "Unable to open configuration file.";
 		}
-		my $config_hash_ref = read_config($config_file_path);
-		print Dumper $config_hash_ref;		#ToDo: remove it after debug completed.
-		
+		$config_hash_ref = read_config($config_file_path);
+		print Dumper $config_hash_ref;    #ToDo: remove it after debug completed.
+
 		my $msg;
 		unless ( check_is_config_valid( $config_hash_ref, \$msg ) ) {
 			error $msg;
@@ -169,7 +286,13 @@ sub main() {
 		unless ( defined $config_hash_ref->{'Host'} ) {
 			error "Host is not specified.";
 		}
-		run_daemon($config_hash_ref);
+		run_daemon();
+	}
+	elsif ($install) {
+		install();
+	}
+	elsif ($uninstall) {
+		uninstall();
 	}
 	else {
 		exit print_usage();
@@ -177,3 +300,71 @@ sub main() {
 }
 
 main();
+
+=head1 NAME
+
+ftp-dir-syncd - Crossplatform daemon/service to keep local and remote(accessible via ftp) directories in sync
+
+=head1 SYNOPSIS
+
+ftp-dir-syncd.pl [options]
+
+Options:
+	
+	--help          to view help
+	
+	--man           to view full documentation
+	
+	--version       to print version
+	
+	--config=<file> to specify custom path to file
+	
+	--daemon        to start as daemon/service
+	
+Windows only options:
+	
+	--install       to install service
+	
+	--uninstall     to uninstall service
+	
+	
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--help>
+
+TBD.
+
+=item B<--man>
+
+TBD.
+
+=item B<--version>
+
+TBD.
+
+=item B<--config>
+
+TBD.
+
+=item B<--daemon>
+
+TBD.
+
+=item B<--install>
+
+TBD.
+
+=item B<--uninstall>
+
+TBD.
+
+=back
+
+=head1 DESCRIPTION
+
+B<This daemon/service> work in background to keep directories in sync
+
+=cut
